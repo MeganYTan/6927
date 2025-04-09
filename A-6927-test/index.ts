15c2289d39a22076a16ca8aeb52d7be0d7e479ef
Megan Tan <mtan@Megans-MacBook-Pro.local>
2025-04-09 16:12:25 -0500

    Add child_process to check if targeted stack exists

--- A-6927-test/index.ts
+++ A-6927-test/index.ts
@@ -1,37 +1,45 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure-native";
+import * as child_process from "child_process";
 
-const stackRef = new pulumi.StackReference("mtan/B-6927-test/dev");
-console.log("Stack ref: ", stackRef);
+const targetedStackName = "mtan/B-6927-test/dev";
+console.log(`Targeted Stack: ${targetedStackName}`);
+
+function stackExists(targetedStackName: string): boolean {
+  try {
+    child_process.execSync(
+      `pulumi stack select ${targetedStackName} --non-interactive`,
+      {
+        stdio: "ignore",
+      }
+    );
+    return true;
+  } catch {
+    return false;
+  }
+}
+console.log(`Does stack ${targetedStackName} exist?: ${stackExists(targetedStackName)}`);
 
 async function main() {
-    if (!stackRef) {
-        console.log("Stack B-6927-test does not exist yet. Will retry on next deployment.");
-        return;
-    }
-    // Check if the stack reference exists
-    try {
-        const frontDoorId = stackRef.getOutput("frontDoorId");
-
-        const resourceGroupA = new azure.resources.ResourceGroup("example-rg-a");
-
-        const storageAccount = new azure.storage.StorageAccount("examplestorage", {
-            resourceGroupName: resourceGroupA.name,
-            sku: {
-                name: "Standard_LRS"
-            },
-            kind: "StorageV2"
-        });
-
-        const storageContainer = new azure.storage.BlobContainer("examplecontainer", {
-            resourceGroupName: resourceGroupA.name,
-            accountName: storageAccount.name,
-            publicAccess: azure.storage.PublicAccess.None
-        });
-
-    } catch (error) {
-        console.log("Stack B-6927-test does not exist yet. Will retry on next deployment.");
-    }
+  if (!stackExists(targetedStackName)) {
+    console.log(`${targetedStackName} does not exist. Will retry on next deployment.`);
+    return;
+  }
+  const stackRef = new pulumi.StackReference(targetedStackName);
+  const frontDoorId = stackRef.getOutput("frontDoorId");
+  const resourceGroupA = new azure.resources.ResourceGroup("example-rg-a");
+  const storageAccount = new azure.storage.StorageAccount("examplestorage", {
+    resourceGroupName: resourceGroupA.name,
+    sku: {
+      name: "Standard_LRS",
+    },
+    kind: "StorageV2",
+  });
+  const storageContainer = new azure.storage.BlobContainer("examplecontainer", {
+    resourceGroupName: resourceGroupA.name,
+    accountName: storageAccount.name,
+    publicAccess: azure.storage.PublicAccess.None,
+  });
 }
 
 main();
